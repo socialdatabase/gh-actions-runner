04b679f4eeea0c264c3a0714165f2e5733add40a
Chiel Heldoorn <chiel_heldoorn@hotmail.com>
2022-11-24 14:44:12 +0100

    fix hooks and chwon

--- setup.sh
+++ setup.sh
@@ -22,7 +22,7 @@ echo "$ROW" | sudo tee -a /etc/fstab
 # install docker using instructions from official website: https://docs.docker.com/engine/install/ubuntu/#install-using-the-convenience-script 
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
-sudo chown $(whoami):$(whoami) /var/run/docker.sock
+sudo chown $USER:$USER /var/run/docker.sock
 
 # install docker-compose v1 from source, v2 (using `docker compose` instead of `docker-compose`) is already installed with the installation of docker
 sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
@@ -37,7 +37,15 @@ sudo systemctl restart docker
 # INSTALL DEPENDENCIES AND START RUNNER
 curl -s https://raw.githubusercontent.com/actions/runner/main/src/Misc/layoutbin/installdependencies.sh | DEBIAN_FRONTEND=noninteractive sudo bash
 curl -s https://raw.githubusercontent.com/actions/runner/main/scripts/create-latest-svc.sh | bash -s socialdatabase
-sudo chown -R $(whoami):$(whoami) ./runner/  # https://github.com/actions/checkout/issues/211#issuecomment-611986243
+sudo chown -R $USER:$USER ./runner/  # https://github.com/actions/checkout/issues/211#issuecomment-611986243
 
 # CONFIGURE ACTIONS_RUNNER_HOOK_JOB_COMPLETED
-echo "ACTIONS_RUNNER_HOOK_JOB_COMPLETED='"'[[ ! -z $(docker ps -a -q) ]] && docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'"'" >> "$HOME"/runner/.env
+sudo tee $HOME/cleanup.sh <<"EOF"
+#!/usr/bin/env bash
+[[ ! -z $(docker ps -a -q) ]]:
+    docker stop $(docker ps -a -q)
+    docker rm $(docker ps -a -q)
+    docker volume rm $(docker volume ls -qf dangling=true)
+EOF
+
+echo "ACTIONS_RUNNER_HOOK_JOB_COMPLETED=$HOME/cleanup.sh" >> "$HOME"/runner/.env
