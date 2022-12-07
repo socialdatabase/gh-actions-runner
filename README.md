040ca5ecb0ef1a4a81da952f3bd0ac12d204f04d
Chiel Heldoorn <chiel_heldoorn@hotmail.com>
2022-12-07 18:39:22 +0100

    fix noninteractivity of script

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 For running the workflows on our own infrastructure. This is handy because of:
 - Speed, docker builds are cached
 - Costs, buying minutes from GitHub is more expensive
+
+
+```
+curl -s https://raw.githubusercontent.com/socialdatabase/gh-actions-runner/master/setup-ubuntu-dc-vm.sh | RUNNER_CFG_PAT=ghp_*** DEBIAN_FRONTEND=noninteractive bash
+```
--- setup-ubuntu-dc-vm.sh
+++ setup-ubuntu-dc-vm.sh
@@ -2,7 +2,8 @@
 set -e
 
 [ -z "$RUNNER_CFG_PAT" ] && echo "Need to set RUNNER_CFG_PAT" && exit 1;
-DEBIAN_FRONTEND=noninteractive sudo apt -y install curl jq
+export DEBIAN_FRONTEND=noninteractive
+sudo apt -y install curl jq
 
 # INSTALL DOCKER
 # install docker using instructions from official website: https://docs.docker.com/engine/install/ubuntu/#install-using-the-convenience-script 
@@ -11,12 +12,12 @@ sudo sh get-docker.sh
 sudo chown $USER:$USER /var/run/docker.sock
 
 # install docker-compose v1 from source, v2 (using `docker compose` instead of `docker-compose`) is already installed with the installation of docker
-sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo --preserve-env=DEBIAN_FRONTEND curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo --preserve-env=DEBIAN_FRONTEND chmod +x /usr/local/bin/docker-compose
+sudo --preserve-env=DEBIAN_FRONTEND ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 # INSTALL DEPENDENCIES AND START RUNNER
-curl -s https://raw.githubusercontent.com/actions/runner/main/src/Misc/layoutbin/installdependencies.sh | DEBIAN_FRONTEND=noninteractive sudo bash
+curl -s https://raw.githubusercontent.com/actions/runner/main/src/Misc/layoutbin/installdependencies.sh | sudo --preserve-env=DEBIAN_FRONTEND --preserve-env=RUNNER_CFG_PAT bash
 curl -s https://raw.githubusercontent.com/actions/runner/main/scripts/create-latest-svc.sh | bash -s socialdatabase
 sudo chown -R $USER:$USER ./runner/  # https://github.com/actions/checkout/issues/211#issuecomment-611986243
 
@@ -35,7 +36,7 @@ sudo chmod +x cleanup.sh
 sudo tee $HOME/startup.sh <<"EOF"
 #!/usr/bin/env bash
 echo "startup hook"
-sudo chown -R ubuntu:ubuntu /home/ubuntu/runner/
+sudo chown -R mysd:mysd /home/mysd/runner/
 EOF
 echo "ACTIONS_RUNNER_HOOK_JOB_STARTED=$HOME/startup.sh" >> "$HOME"/runner/.env
 sudo chmod +x $HOME/startup.sh
